1a5036854d6def39e3d034c1ae62a0d7cec75081
Chris Day <prototype625@gmail.com>
2016-09-16 20:17:01 -0600

    added tons of things - not very gitty

--- denvercrawler.py
+++ denvercrawler.py
@@ -5,12 +5,16 @@ from bs4 import BeautifulSoup as bs
 from requests import ConnectionError
 import requests
 import ssl
-#from dbhandler import *
+import os
+import shutil
 import time
+import urllib2
 
+#dummy headers to make pages trust our evil crawler
 headers={'Accept-Encoding': '*','Accept': '*/*', 'User-Agent': 'Mozilla 5.0'}
 ssl._create_default_https_context = ssl._create_unverified_context
 
+#this is the page we want to scrape and soupify
 def GetPage():
     url="https://www.denvergov.org/content/denvergov/en/denver-development-services/help-me-find-/building-permits.html"
     req = requests.get(url,headers=headers)
@@ -18,9 +22,27 @@ def GetPage():
     soup = bs(page,"lxml")
     return soup
 
-page = GetPage()
-links = page.find_all('a',href=True)
-for link in links:
-    path=link.get('href')
-    if "xls" in path:
-        print path
+#let's find .xls on this page
+def GetLinks():
+    page = GetPage()
+    links = page.find_all('a',href=True)
+    for link in links:
+        path=link.get('href')
+        if "xls" in path:
+            filename=link.get_text().replace(" ", "").replace("/","-")+".xls"
+            if "http" in path:
+                SaveFile(path,filename)
+            else:
+                domain="https://www.denvergov.org"
+                path=domain+path
+                SaveFile(path,filename)
+
+#save those files!
+def SaveFile(path,filename):
+    req = urllib2.urlopen(path)
+    data = req.read()
+    file_ = open(filename, 'w')
+    file_.write(data)
+    file_.close()
+
+GetLinks()
